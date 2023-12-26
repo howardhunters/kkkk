62ef1194d05a9eb928be2f04d3ef121721aeb900
howardhunters <154858545+howardhunters@users.noreply.github.com>
2023-12-26 16:12:56 +1200

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'2f9ab0f2-7796-4440-b629-97407095bc1b'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
