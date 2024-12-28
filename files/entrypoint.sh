eb27b81365fdc22e9d2dde8489c70c68cf87b656
howardhunters <154858545+howardhunters@users.noreply.github.com>
2024-12-28 19:23:56 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -330,7 +330,8 @@ check_variable() {
 # 下载最新版本 Nezha Agent
 download_agent() {
   if [ ! -e nezha-agent ]; then
-    wget -qO "https://github.com/nezhahq/agent/releases/download/v0.20.5/nezha-agent_freebsd_amd64.zip"
+    URL="https://github.com/nezhahq/agent/releases/download/v0.20.5/nezha-agent_linux_amd64.zip"
+    wget ${URL}
     unzip -qod ./ nezha-agent_linux_amd64.zip
     rm -f nezha-agent_linux_amd64.zip
   fi
